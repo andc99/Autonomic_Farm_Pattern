566fbb278bc5de5f2eee8301508053892127bcbd
Brotherhood94 <alessandro.berti94@hotmail.it>
2019-08-11 19:03:25 +0200

    ref

--- afterSF/autonomic_farm.cpp
+++ afterSF/autonomic_farm.cpp
@@ -1,8 +1,12 @@
 #include "autonomic_farm.h"
 
-ProcessingElement::ProcessingElement(){
+ProcessingElement::ProcessingElement(bool sticky){
+	this->sticky = sticky;
 	static std::atomic<unsigned int> id{0};
 	this->thread_id = id++; // (std::thread::hardware_concurrency);
+	std::cout << "ID: " << this->thread_id << std::endl;
+	if(this->sticky)	
+		move_to_context(this->get_id());
 	std::cout << "Machine Hardware Concurrency " << std::thread::hardware_concurrency << std::endl;
 	return;
 }
@@ -41,3 +45,91 @@ unsigned int ProcessingElement::move_to_context(unsigned int id_context){
 //	Emitter
 //
 /////////////////////////////////////////////////////////////////////////
+template <typename I>
+Emitter<I>::Emitter(std::vector<Circular_Buffer*>* win_cbs, std::vector<I>* collection, bool sticky) : ProcessingElement(sticky){
+	this->win_cbs = win_cbs;
+	this->emitter_cb = new Circular_Buffer(50); // da cambiare!
+	this->collection = collection;
+}
+
+
+template <typename I>
+void Emitter<I>::body(){
+	unsigned int id_queue{0};
+	for(auto i = 0; i < (*collection).size(); i++){
+		I &task = (*collection)[i];
+		(*win_cbs)[id_queue]->safe_push(&task); //safe_try!!!!
+		id_queue = (id_queue++)%win_cbs->size();
+		std::cout << "ID_QUEUE EMITTER: " << id_queue << std::endl;
+	}
+	for(auto i = 0; i < win_cbs->size(); i++)
+		(*win_cbs)[i]->safe_push(EOS); // qui non ci può andare la safe_try
+	return;
+}
+
+
+template <typename I>
+void Emitter<I>::run(){
+	this->thread = new std::thread(&Emitter<I>::body, this);
+	return;
+}
+
+template <typename I>
+Circular_Buffer* Emitter<I>::get_in_queue(){
+	return this->emitter_cb;
+}
+
+
+/////////////////////////////////////////////////////////////////////////
+//
+//	Worker	
+//
+/////////////////////////////////////////////////////////////////////////
+
+
+template <typename I, typename O>
+Worker<I,O>::Worker(std::function<I(O)> fun_body, bool sticky):ProcessingElement(sticky){
+	this->fun_body = fun_body;
+	this->win_cb = new Circular_Buffer(50); /////////yyp
+	this->wout_cb = new Circular_Buffer(50); /////////yyp
+};
+
+
+template <typename I, typename O>
+void Worker<I,O>::body(){
+	void* task = 0;
+	win_cb->safe_pop(&task);
+	while( (I*) task != EOS){ //NON SICURO FUNZIONI
+		std::cout << "entroo " <<std::endl;
+		I &t = (*((I*) task));
+		O res = fun_body(t);
+		this->wout_cb->safe_push(&res); //safe_try
+		win_cb->safe_pop(&task);	
+	};
+	this->wout_cb->safe_push(EOS);
+	return;
+}
+
+template <typename I, typename O>
+void Worker<I,O>::run(){
+	this->thread = new std::thread(&Worker<I,O>::body_worker, this);
+	return;
+}
+
+
+template <typename I, typename O>
+Circular_Buffer* Worker<I,O>::get_in_queue(){
+	return this->win_cb;
+}
+template <typename I, typename O>
+Circular_Buffer* Worker<I,O>::get_out_queue(){
+	return this->wout_cbs;
+}
+
+
+
+
+
+
+
+
--- afterSF/autonomic_farm.h
+++ afterSF/autonomic_farm.h
@@ -3,17 +3,21 @@
 #include <stdlib.h>
 #include <iostream>
 #include <math.h>
+#include <vector>
+#include <functional>
 
-#define EOS -1
+#define EOS (void*)-1
 
 //gestire gli IFNOTDEF, ad esempio IFNOTDEF circular_buffer, define!
 
 class ProcessingElement{
 	protected:
+		bool sticky;
 		unsigned int thread_id;
 		std::thread* thread;
-		ProcessingElement();
+		ProcessingElement(bool sticky);
 		~ProcessingElement();
+		virtual void body() = 0;
 		virtual void run() = 0;
 
 	public:
@@ -26,5 +30,30 @@ class ProcessingElement{
 
 template<class I>
 class Emitter : public ProcessingElement{
+	private:
+		std::vector<Circular_Buffer*>* win_cbs; //input queues to workers
+		Circular_Buffer* emitter_cb; //potrebbe non essere necessario, dovrebbero esserfe concatenabili (?)
+		std::vector<I>* collection;
 
-}
+	public:
+		Emitter(std::vector<Circular_Buffer*>* win_cbs, std::vector<I>* collection, bool sticky);//Super??
+		void body();
+		void run();
+		Circular_Buffer* get_in_queue();
+};
+
+template<class I, class O>
+class Worker : public ProcessingElement{
+	private:
+		std::function<I(O)> fun_body;
+		Circular_Buffer* win_cb;
+		Circular_Buffer* wout_cb;
+
+	public: 
+		Worker(std::function<I(O)> fun_body, bool sticky);
+		void body();
+		void run();
+		Circular_Buffer* get_in_queue();
+		Circular_Buffer* get_out_queue();
+
+};
